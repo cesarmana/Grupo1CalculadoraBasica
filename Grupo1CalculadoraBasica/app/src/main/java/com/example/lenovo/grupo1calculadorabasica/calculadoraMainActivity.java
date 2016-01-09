4411658e3268b346e3a34ab2733ee0fdbf6e9db0
cocoriotos <cocoriotos@hotmail.com>
2016-01-09 15:02:40 -0500

    Adding Dividing and Cero Button
    By Julián González

--- Grupo1CalculadoraBasica/app/src/main/java/com/example/lenovo/grupo1calculadorabasica/calculadoraMainActivity.java
+++ Grupo1CalculadoraBasica/app/src/main/java/com/example/lenovo/grupo1calculadorabasica/calculadoraMainActivity.java
@@ -1,5 +1,9 @@
-package com.example.lenovo.grupo1calculadorabasi
+package com.example.lenovo.grupo1calculadorabasica;
+// Realizado por: Yudy Alarcón
+//                 Julián González
+//                 César M Naranjo
 
+//Inicio Código César M Alarcón
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -177,8 +181,6 @@ public class calculadoraMainActivity extends AppCompatActivity {
     }//fin metodo menos
 
 
-
-
     //
     public double operacion(int caso, double ope1, double ope2){
 
@@ -233,4 +235,199 @@ public class calculadoraMainActivity extends AppCompatActivity {
     public void setResultado2(double resultado2) {
         this.resultado2 = resultado2;
     }
+
+//**
+// Fín Código César M Naranjo
+
+//Inicio Código Yudy
+public void multiplicacion(View v) {
+
+    //validamos si el campo operador1 esta vacio  y el usuario oprimio la tecla *
+    if (operador1.getText().toString().trim().length() < 1) {
+        setCaso(3);
+
+        //Proceso para cambiar el ultimo digito de la cadena por el signo *
+        String cadena2;
+        int num_cadena2;
+        cadena2=getCadena();
+        num_cadena2=cadena2.length();
+
+
+        if(num_cadena2>1){
+
+            char[]cad = cadena2.toCharArray();
+            cad[num_cadena2-1] = '*';
+            cadena2 = new String(cad);
+            setCadena(cadena2);
+
+            //se muestra la cadena que representa la operacion matematica
+            operacionmat.setText(cadena);
+        }
+
+    } else {
+
+        double ope1, ope2;
+        String cadena;
+
+        //convertimos el texto obtenido a tipo numerico double
+        ope1 = Double.parseDouble(operador1.getText().toString());
+        ope2 = Double.parseDouble(resultado.getText().toString());
+
+        //Vamos armando la cadena que representa la operacion matematica
+        cadena = getCadena();
+        if (cadena == null) {
+            cadena = String.valueOf(ope1) + "*";
+            setCadena(cadena);
+        } else {
+            cadena = cadena + String.valueOf(ope1) + "*";
+            setCadena(cadena);
+        }
+        //que numero de caso va
+        int caso = getCaso();
+
+        //se valida si es la primera vez que se ejecuta la operacion
+        if (caso == 0) {
+            //cambiamos el cero por uno para saber que la operacion que esta iniciada es una suma
+            setCaso(3);
+
+            setResultado2(ope1);
+
+        } else {
+            //se hace la respectiva operacion
+            setResultado2(operacion(caso,ope1,ope2));
+
+        }
+
+        //se imprime por pantalla el resultado
+        resultado.setText(String.valueOf(getResultado2()));
+
+        //se limpia el display para un nuevo operador
+        operador1.setText("");
+
+        //se muestra la cadena que representa la operacion matematica
+        operacionmat.setText(cadena);
+
+        //el usuario pidio multiplicacion
+        setCaso(3);
+    }
+}//fin metodo multiplicacion
+
+    //**
+//
+    public void igual(View v){
+
+        switch(getCaso()){
+            case 0:
+                mas(v);
+                break;
+            case 1:
+                mas(v);
+                break;
+            case 2:
+                menos(v);
+                break;
+            case 3:
+                multiplicacion(v);
+                break;
+            case 4:
+                //division(v);//quitar comentario cuando se cree el metodo division
+            default:
+                break;
+
+        }
+    }//fin metodo igual
+    //fin codigo Yudy Alarcón
+
+// Inicio Código Julián González
+
+    public void division (View v) {
+
+        //validamos si el campo operador1 esta vacio  y el usuario oprimio la tecla /
+        if (operador1.getText().toString().trim().length() < 1) {
+            setCaso(4);
+
+            //Proceso para cambiar el ultimo digito de la cadena por el signo /
+            String cadena2;
+            int num_cadena2;
+            cadena2=getCadena();
+            num_cadena2=cadena2.length();
+
+
+            if(num_cadena2>1){
+
+                char[]cad = cadena2.toCharArray();
+                cad[num_cadena2-1] = '/';
+                cadena2 = new String(cad);
+                setCadena(cadena2);
+
+                //se muestra la cadena que representa la operacion matematica
+                operacionmat.setText(cadena);
+            }
+
+        } else {
+
+            double ope1, ope2;
+            String cadena;
+
+            //convertimos el texto obtenido a tipo numerico double
+            ope1 = Double.parseDouble(operador1.getText().toString());
+            ope2 = Double.parseDouble(resultado.getText().toString());
+
+            //Vamos armando la cadena que representa la operacion matematica
+            cadena = getCadena();
+            if (cadena == null) {
+                cadena = String.valueOf(ope1) + "/";
+                setCadena(cadena);
+            } else {
+                cadena = cadena + String.valueOf(ope1) + "/";
+                setCadena(cadena);
+            }
+            //que numero de caso va
+            int caso = getCaso();
+
+            //se valida si es la primera vez que se ejecuta la operacion
+            if (caso == 0) {
+                //cambiamos el cero por cuatro para saber que la operacion que esta iniciada es una division
+                setCaso(4);
+
+                setResultado2(ope1);
+
+            } else {
+                //se hace la respectiva operacion
+                setResultado2(operacion(caso,ope1,ope2));
+
+            }
+
+            //se imprime por pantalla el resultado
+            resultado.setText(String.valueOf(getResultado2()));
+
+            //se limpia el display para un nuevo operador
+            operador1.setText("");
+
+            //se muestra la cadena que representa la operacion matematica
+            operacionmat.setText(cadena);
+
+            //el usuario pidio division
+            setCaso(4);
+        }
+    }//fin metodo division
+
+    //Boton nuevo (C)
+    public void nuevo (View v){
+
+        //se imprime por pantalla el resultado
+        resultado.setText(String.valueOf(""));
+
+        //se limpia el display para un nuevo operador
+        operador1.setText("");
+
+        //se muestra la cadena que representa la operacion matematica
+        operacionmat.setText("");
+
+        setCadena("");
+        setCaso(0);
+        setResultado2(0);
+
+    }//fin metodo nuevo
+    // Fin Código Julián González
 }
